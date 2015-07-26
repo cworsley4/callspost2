77f53d68ab5bf2ad144f2a389717e3276f1213d6
Cecil Worsley <cecil@looker.com>
2015-07-26 14:35:59 -0700

    saving go work, changing course for a faster prototype in javascript

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
 import "fmt"
+import "time"
+// import revel "github.com/revel/revel"
+import twiliogo "github.com/carlosdp/twiliogo"
+
+twilioClientId := 
 
 func main() {
-	fmt.Println("Hello, 世界")
+
+  client := twilio.NewClient("<ACCOUNT_SID>", "<AUTH_TOKEN>")
+  var companies = []string{"+18007779898", "+18008924357"}
+
+  for i := 0; i < len(companies); i++ {
+    var phone = companies[i]
+    call, err := twilio.NewCall(client, phone, "+16502354600", nil)
+
+    timer := time.NewTimer(time.Minute)
+
+    go func() {
+      <-timer.C
+      fmt.Println("Timer 2 expired")
+    }()
+  }
+
+  select{}
 }
 
